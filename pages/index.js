5f77619cc9a9f93702abbc1b766277f9be69f574
Khalid Adan <khalid.adan@gmail.com>
2021-11-25 15:04:39 -0500

    First pass at design done, added responsive breakpoints, colour palette is WIP

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,49 @@
-export default function Home() {
+export default function Index() {
   return (
-    <div className="container mx-auto space-x-4 flex flex-rows mt-8">
-      <button className="btn btn-default">Default</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-danger">Danger</button>
-      <button className="btn btn-link">Link</button>
+    <div className="container mx-auto flex flex-col mt-8 font-gc space-y-4">
+      <div className="grid tablet:grid-cols-1 mobile:grid-cols-2 gap-4">
+        <div className="rounded border-[1px] border-solid border-default-text px-6 py-4 space-y-4">
+          <p className="h5">Inputs</p>
+          <input type="text" placeholder="name" className="form-control" />
+          <input type="number" placeholder="5" className="form-control" />
+          <input
+            type="email"
+            placeholder="my@email.com"
+            className="form-control"
+          />
+          <input type="password" placeholder="****" className="form-control" />
+          <select className="form-control" placeholder="Select from ...">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="2">3</option>
+            <option value="2">4</option>
+            <option value="2">5</option>
+          </select>
+          <p className="italic">Searchable Select? Mockup required</p>
+        </div>
+        <div className="rounded border-[1px] border-solid border-default-text px-6 py-4">
+          <p className="h5">Typography</p>
+          <p className="h1">Size H1</p>
+          <p className="h2">Size H2</p>
+          <p className="lead">Size Lead</p>
+          <p className="h3">Size H3</p>
+          <p className="h4">Size H4</p>
+          <p className="h5">Size H5</p>
+          <p className="">(Default Size)</p>
+          <p className="h6">Size H6</p>
+          <p className="small">Size small</p>
+        </div>
+      </div>
+      <div className="rounded border-[1px] border-solid border-default-text px-6 py-4 space-x-4">
+        <p className="h5">Buttons</p>
+        <button className="btn btn-default">Default</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-info">Info</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-danger">Danger</button>
+        <button className="btn btn-link">Link</button>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,51 +8,118 @@ module.exports = {
       tablet: '992px',
       mobile: '768px',
     },
+    fontSize: {
+      small: [
+        '87%',
+        {
+          lineHeight: 1.4375,
+        },
+      ],
+      h6: [
+        '14px',
+        {
+          lineHeight: 1.4375,
+        },
+      ],
+      base: [
+        '14px',
+        {
+          lineHeight: 1.4375,
+        },
+      ],
+      h5: [
+        '16px',
+        {
+          lineHeight: 1.1,
+        },
+      ],
+      h4: [
+        '18px',
+        {
+          lineHeight: 1.1,
+        },
+      ],
+      h3: [
+        '22px',
+        {
+          lineHeight: 1.1,
+        },
+      ],
+      lead: [
+        '24px',
+        {
+          lineHeight: 1.1,
+        },
+      ],
+      h2: [
+        '26px',
+        {
+          lineHeight: 1.1,
+        },
+      ],
+      h1: [
+        '34px',
+        {
+          lineHeight: 1.1,
+        },
+      ],
+    },
     colors: {
       'white': '#FFF',
       'black': '#000',
+
       // default
       'default-text': '#335075',
-      'default-bg': '#eaebed',
-      'default-hover-bg': '#cfd1d5',
+      'default': '#eaebed',
+      'default-hover': '#cfd1d5',
       'default-border': '#dcdee1',
 
       // primary - text is white
-      'primary-bg': '#2572b4',
+      'primary': '#2572b4',
       'primary-border': '#091c2d',
-      'primary-hover-bg': '#1c578a;',
+      'primary-hover': '#1c578a;',
 
       // success
-      'success-bg': '#1b6c1c',
+      'success': '#1b6c1c',
       'success-border': '#071a07',
-      'success-hover-bg': '#114311;',
+      'success-hover': '#114311;',
 
       // info
-      'info-bg': '#4d4d4d;',
+      'info': '#4d4d4d;',
       'info-border': '#000',
-      'info-hover-bg': '#343333',
+      'info-hover': '#343333',
 
       // warning
-      'warning-bg': '#f2d40d;',
+      'warning': '#f2d40d;',
       'warning-border': '#917f08',
-      'warning-hover-bg': '#c2aa0a',
+      'warning-hover': '#c2aa0a',
 
       // danger
-      'danger-bg': '#bc3331',
+      'danger': '#bc3331',
       'danger-border': '#942826',
-      'danger-hover-bg': '#3b100f',
+      'danger-hover': '#3b100f',
 
       // link
-      'link-bg': 'transparent',
-      'link-border': 'none',
-      'link-hover-bg': 'none',
+      'transparent': 'transparent',
+
+      // misc
+      'muted': '#555',
+      'form-border': '#ccc',
+      'form-highlighted': '#66afe9;',
     },
     extend: {
+      fontFamily: {
+        gc: 'Helvetica Neue, Helvetica, Arial, sans-serif',
+      },
       minHeight: {
         9: '36px',
       },
       minWidth: {
         9: '36px',
+      },
+      boxShadow: {
+        'active-form':
+          'inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6)',
       },
     },
   },
