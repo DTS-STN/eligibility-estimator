2149fbfc8623e6e81f8f95261a5fa00b82c8248a
Jeremy Kennedy <jerekennedy@deloitte.ca>
2021-11-26 14:24:10 -0500

    update existing js files to ts

--- pages/api/metrics.ts
+++ pages/api/metrics.ts
@@ -1,8 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
 import { register, collectDefaultMetrics } from 'prom-client'
 
 collectDefaultMetrics({ prefix: 'omnidevfrontend_' })
 
-export default function handler(req, res) {
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
   res.setHeader('Content-type', register.contentType)
   res.send(register.metrics())
 }
