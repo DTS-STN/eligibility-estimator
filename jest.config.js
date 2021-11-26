e298d06b04427741e82abe8328aed4da815d17c3
Jeremy Kennedy <jerekennedy@deloitte.ca>
2021-11-26 18:28:08 -0500

    tweak jest implementation

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
+  // custom
+  testMatch: ['**/__tests__/**/*.test.{js,ts}'],
   modulePathIgnorePatterns: ['./cypress'],
+
+  // straight from Next, source: https://nextjs.org/docs/testing#jest-and-react-testing-library
   collectCoverageFrom: [
-    'components/**/*.{js,jsx}',
-    'pages/**/*.{js,jsx}',
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
     '!**/node_modules/**',
+    // custom
+    '!*.config.js',
+    '!.next/**',
+    '!coverage/**',
   ],
   moduleNameMapper: {
     /* Handle CSS imports (with CSS modules)
@@ -14,14 +22,19 @@ module.exports = {
     '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
 
     /* Handle image imports
-      https://jestjs.io/docs/webpack#handling-static-assets */
-    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    https://jestjs.io/docs/webpack#handling-static-assets */
+    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$':
+      '<rootDir>/__mocks__/fileMock.js',
+
+    /* Handle module aliases
+    source: https://github.com/vercel/next.js/blob/canary/examples/with-jest/jest.config.js */
+    '^@/components/(.*)$': '<rootDir>/components/$1',
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   testEnvironment: 'jsdom',
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
-      https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+       https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
   transformIgnorePatterns: [
