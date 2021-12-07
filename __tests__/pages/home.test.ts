f11a2502eff8f1d39a4c6fb74b063e706347c105
Khalid <kadan@deloitte.ca>
2021-12-07 15:17:54 -0500

    Added sanity test for th ehomepage, just making sure it loads correctly for now

---
+++ __tests__/pages/home.test.ts
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Home from '../../pages/index'
+import React from 'react'
+import * as nextRouter from 'next/router'
+
+describe('index page', () => {
+  let useRouter, data
+  // mocking useRouter, as we'll eventually need it for sedning the correct requests
+  beforeAll(() => {
+    useRouter = jest.spyOn(nextRouter, 'useRouter')
+    useRouter.mockImplementation(() => ({
+      route: '/',
+      pathname: '/',
+      query: '',
+      asPath: '',
+    }))
+  })
+
+  it('should render the home page', async () => {
+    // Once API is ready, fetch here
+    render(React.createElement(Home))
+    const main = screen.getByRole('main')
+    expect(main).toBeInTheDocument()
+  })
+})
