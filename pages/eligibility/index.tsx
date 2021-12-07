d2eaf20ee87ab3dd486f5c31ea20f900ca9bd50b
Khalid <kadan@deloitte.ca>
2021-12-07 15:13:44 -0500

    Data fetching from backend working, updated some tests, added swr for clientside data fetching

---
+++ pages/eligibility/index.tsx
@@ -0,0 +1,48 @@
+import { GetServerSideProps, NextPage } from 'next'
+import { Layout } from '../../components/Layout'
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+
+const dataFetcher = async (url) => {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  if (res.status !== 200) {
+    throw new Error(data.message)
+  }
+  return data
+}
+
+const Eligiblity: NextPage = () => {
+  const { query } = useRouter()
+
+  const params = Object.keys(query)
+    .map((key) => key + '=' + query[key])
+    .join('&')
+
+  const { data, error } = useSWR(
+    () => query && `api/calculateEligibility?${params && params}`,
+    dataFetcher
+  )
+
+  if (error)
+    return (
+      <Layout>
+        <div>{error.message}</div>
+      </Layout>
+    )
+  if (!data)
+    return (
+      <Layout>
+        <div>Loading form...</div>
+      </Layout>
+    )
+
+  return (
+    <Layout>
+      <div>content</div>
+    </Layout>
+  )
+}
+
+export default Eligiblity
