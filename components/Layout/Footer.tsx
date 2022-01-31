a5a10f1d857abc010ca44b1c88afb4ca4a0dc6a5
Khalid <kadan@deloitte.ca>
2022-01-31 15:13:32 -0500

    Added footer padding fix

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,61 +1,62 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './Footer.module.css'
 
 export const Footer = () => (
   <footer className="text-[14px]">
     <div
-      className={`w-full h-auto bg-custom-blue-dark px-4 py-8 ${styles.footerBackground}`}
+      className={`w-full h-auto px-4 py-8 bg-custom-blue-dark ${styles.footerBackground}`}
     >
       <div
-        className="py-7 sm:container mx-auto"
+        className="sm:container mx-auto"
         role="navigation"
         aria-labelledby="footerNav1"
       >
         <h3 className="sr-only" id="footerNav1">
           About government
         </h3>
-        <ul className="flex flex-col text-xs lg:grid lg:grid-cols-2 xl:grid xl:grid-cols-3 lg:gap-1">
-          <li className="text-white w-64 md:w-56 lg:w-80 my-2.5 hover:underline">
+        <ul className="flex flex-col text-xs lg:grid lg:grid-cols-3 lg:gap-1">
+          <li className="text-white w-64 md:w-56 lg:w-80 hover:underline">
             <a className="font-body" href="#">
               Contact us
             </a>
           </li>
-          <li className="text-white w-64 md:w-56 lg:w-80 my-2.5 hover:underline">
+          <li className="text-white w-64 md:w-56 lg:w-80 hover:underline">
             <a className="font-body" href="#">
               News
             </a>
           </li>
-          <li className="text-white w-64 md:w-56 lg:w-80 my-2.5 hover:underline">
+          <li className="text-white w-64 md:w-56 lg:w-80 hover:underline">
             <a className="font-body" href="#">
               Prime Minister
             </a>
           </li>
-          <li className="text-white w-64 md:w-56 lg:w-80 my-2.5 hover:underline">
+          <li className="text-white w-64 md:w-56 lg:w-80 hover:underline">
             <a className="font-body" href="#">
               Departments and agencies
             </a>
           </li>
-          <li className="text-white w-64 md:w-56 lg:w-80 my-2.5 hover:underline">
+          <li className="text-white w-64 md:w-56 lg:w-80 hover:underline">
             <a className="font-body" href="#">
               Treaties, laws and regulations
             </a>
           </li>
-          <li className="text-white w-64 md:w-56 lg:w-80 my-2.5 hover:underline">
+          <li className="text-white w-64 md:w-56 lg:w-80 hover:underline">
             <a className="font-body" href="#">
-              How government works
+              About government
             </a>
           </li>
-          <li className="text-white w-64 md:w-56 lg:w-80 my-2.5 hover:underline">
+          <li className="text-white w-64 md:w-56 lg:w-80 hover:underline">
             <a className="font-body" href="#">
               Public service and military
             </a>
           </li>
-          <li className="text-white w-64 md:w-56 lg:w-80 my-2.5 hover:underline">
+          <li className="text-white w-64 md:w-56 lg:w-80 hover:underline">
             <a className="font-body" href="#">
               Government-wide reporting
             </a>
           </li>
-          <li className="text-white w-64 md:w-56 lg:w-80 my-2.5 hover:underline">
+          <li className="text-white w-64 md:w-56 lg:w-80 hover:underline">
             <a className="font-body" href="#">
               Open government
             </a>
@@ -64,23 +65,20 @@ export const Footer = () => (
       </div>
     </div>
     <div className="flex justify-between px-4 py-8 sm:container mx-auto">
-      <ul className="flex flex-col md:flex-row md:space-x-6 list-disc px-4">
+      <ul className="flex flex-col md:flex-row md:space-x-6 list-disc px-4 text-[#284162]">
         <li>
           <a href="#">Social media</a>
         </li>
-
+        #284162
         <li>
           <a href="#">Mobile applications</a>
         </li>
-
         <li>
           <a href="#">About Canada.ca</a>
         </li>
-
         <li>
           <a href="#">Terms and conditions</a>
         </li>
-
         <li>
           <a href="#">Privacy</a>
         </li>
@@ -95,24 +93,28 @@ export const Footer = () => (
       </div>
     </div>
     <div className="flex justify-between">
-      <div className="flex items-center md:hidden py-8 md:pt-0">
-        <p className="px-4">Top of page</p>
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M5 15l7-7 7 7"
-            />
-          </svg>
-        </span>
+      <div className="md:hidden py-8 md:pt-0">
+        <Link href="#elig" passHref>
+          <a className="flex items-center">
+            <p className="px-4">Top of page</p>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M5 15l7-7 7 7"
+                />
+              </svg>
+            </span>
+          </a>
+        </Link>
       </div>
       <div className="flex items-center px-4 md:hidden">
         <Image
