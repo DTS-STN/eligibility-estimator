932ab3834880b3f7188827c05d9aa75b7f60a271
Khalid <kadan@deloitte.ca>
2022-01-31 14:55:30 -0500

    Updated copy

--- components/ContactCTA/index.tsx
+++ components/ContactCTA/index.tsx
@@ -4,21 +4,32 @@ import { Dispatch } from 'react'
 export const ContactCTA: React.VFC<{ setSelectedTab: Dispatch<any> }> = ({
   setSelectedTab,
 }) => (
-  <p className="!mt-6 !md:mt-8">
-    For a more accurate assessment, you are encouraged to{' '}
+  <>
+    <p className="!mt-6 !md:mt-8">
+      For a more accurate assessment, you are encouraged to{' '}
+      <Link
+        href="https://www.canada.ca/en/employment-social-development/corporate/contact/oas.html"
+        passHref
+      >
+        <a className="text-default-text underline">contact Service Canada</a>
+      </Link>{' '}
+      and check out the{' '}
+      <span
+        className="underline text-default-text cursor-pointer"
+        onClick={(e) => setSelectedTab(2)}
+      >
+        FAQ
+      </span>{' '}
+      on documents you may be required to provide.
+    </p>
+    <h2 className="h2 mt-8">Next steps</h2>
     <Link
-      href="https://www.canada.ca/en/employment-social-development/corporate/contact/oas.html"
+      href="https://www.canada.ca/en/services/benefits/publicpensions/cpp/old-age-security/apply.html"
       passHref
     >
-      <a className="text-default-text underline">contact Service Canada</a>
-    </Link>{' '}
-    and check out the{' '}
-    <span
-      className="underline text-default-text cursor-pointer"
-      onClick={(e) => setSelectedTab(2)}
-    >
-      FAQ
-    </span>{' '}
-    on documents you may be required to provide.
-  </p>
+      <a className="btn btn-primary w-96" target="_blank">
+        Click here to apply
+      </a>
+    </Link>
+  </>
 )
