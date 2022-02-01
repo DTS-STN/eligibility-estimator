bf48da55a41e390c244142c85c0ed106cc206e54
Khalid <kadan@deloitte.ca>
2022-02-01 09:23:05 -0500

    Re-added some removed code that fixes form highlighting

--- components/Forms/ComponentFactory.tsx
+++ components/Forms/ComponentFactory.tsx
@@ -86,7 +86,7 @@ export const ComponentFactory: React.VFC<FactoryProps> = observer(
             ]}
           />
         )}
-        <div className="grid grid-cols-1 md:grid-cols-3 md:gap-10 mt-10 focus-visible:outline-none">
+        <div className="grid grid-cols-1 md:grid-cols-3 md:gap-10 mt-10">
           <form
             name="ee-form"
             data-testid="ee-form"
