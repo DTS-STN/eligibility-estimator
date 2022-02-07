5bc9f210003d57d369259dc4e2079989891f121f
Jeremy Kennedy <jerekennedy@deloitte.ca>
2022-02-07 13:58:51 -0500

    [wip] dangerouslySetInnerHTML for results table
    todo: text needs to be blue when it's a link

--- components/ResultsTable/EligibilityDetails.tsx
+++ components/ResultsTable/EligibilityDetails.tsx
@@ -8,9 +8,11 @@ export const EligibilityDetails: React.VFC<{
 }> = ({ eligibilityType }) => (
   <>
     {eligibilityType.eligibility.detail.split('\n').map((str, i) => (
-      <p key={i} className={i == 0 ? `inline` : ''}>
-        {str}
-      </p>
+      <p
+        key={i}
+        className={i == 0 ? `inline` : ''}
+        dangerouslySetInnerHTML={{ __html: str }}
+      />
     ))}
   </>
 )
