a8867421243e1fbd29f0f3dbd04b53880df1b774
Khalid Adan <KhalidAdan@users.noreply.github.com>
2022-02-08 16:41:58 -0500

    Update EligibilityDetails.tsx

--- components/ResultsTable/EligibilityDetails.tsx
+++ components/ResultsTable/EligibilityDetails.tsx
@@ -10,7 +10,7 @@ export const EligibilityDetails: React.VFC<{
     {eligibilityType.eligibility.detail.split('\n').map((str, i) => (
       <p
         key={i}
-        className={i == 0 ? 'inline question-link' : 'question-link'}
+        className={i == 0 ? 'inline summary-link' : 'summary-link'}
         dangerouslySetInnerHTML={{ __html: str }}
       />
     ))}
