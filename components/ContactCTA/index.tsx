8fff26e620a3b9c8fdd16776d5cc81b8c1621943
Khalid <kadan@deloitte.ca>
2022-02-15 13:14:01 -0500

    Added eventlistener for FAQ link

--- components/ContactCTA/index.tsx
+++ components/ContactCTA/index.tsx
@@ -1,13 +1,28 @@
 import Link from 'next/link'
+import { useLayoutEffect } from 'react'
 import { WebTranslations } from '../../i18n/web'
 import { useStore, useTranslation } from '../Hooks'
 
 export const ContactCTA: React.VFC = () => {
   const root = useStore()
   const tsln = useTranslation<WebTranslations>()
+
+  const handleClick = () => {
+    root.setActiveTab(2)
+  }
+
+  useLayoutEffect(() => {
+    const self = document.querySelectorAll('#faqLink a')[1]
+    if (!self) return
+
+    self.addEventListener('click', handleClick)
+    return () => self && self.removeEventListener('click', handleClick)
+  })
+
   return (
     <>
       <p
+        id="faqLink"
         className="!mt-6 !md:mt-8 summary-link"
         dangerouslySetInnerHTML={{ __html: tsln.contactCTA }}
       />
