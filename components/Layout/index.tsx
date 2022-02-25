245283930486020ad7f34b494bd8959f3dc2daf4
Khalid <kadan@deloitte.ca>
2022-02-25 11:29:43 -0500

    bold fixes

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -23,11 +23,14 @@ export const Layout: React.VFC<{
         <title>{tsln.title}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Noto+Sans&family=Patua+One&display=swap"
-          rel="stylesheet"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
         ></link>
       </Head>
       <SCLabsTestHeader />
