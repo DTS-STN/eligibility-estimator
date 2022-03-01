c70c04ea779f697f2f88bab321035aba0b09f3a7
Jeremy Kennedy <jerekennedy@deloitte.ca>
2022-03-01 16:58:33 -0500

    update text for french "Get Results" button

--- i18n/web/fr.ts
+++ i18n/web/fr.ts
@@ -30,7 +30,7 @@ const fr: WebTranslations = {
   needHelp: "Besoin d'aide?",
   faq: 'Foire Aux Questions',
   saveToCsv: 'Télécharger',
-  getResults: 'Obtenir des résultats',
+  getResults: 'Obtenir les résultats',
   required: 'obligatoire',
   homePageP1:
     "L'Estimateur Canadien des Prestations de Vieillesse est un prototype en construction. À partir des renseignements que vous fournissez, cet outil estime votre admissibilité à la Sécurité de la vieillesse (SV) et au Supplément de revenu garanti (SRG). Si vous êtes admissible à la prestation, il estime également votre paiement mensuel.",
