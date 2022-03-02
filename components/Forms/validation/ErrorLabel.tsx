159ebfeb72cab367a72f93c69dda58054c5e2d49
Khalid <kadan@deloitte.ca>
2022-03-02 14:46:11 -0500

    Updated margins for error labels

--- components/Forms/validation/ErrorLabel.tsx
+++ components/Forms/validation/ErrorLabel.tsx
@@ -3,7 +3,7 @@ export const ErrorLabel: React.VFC<{ errorMessage: string }> = ({
 }) => (
   <div
     role="alert"
-    className={`border-l-4 border-[#b6070a] opacity-90 px-3 py-1 bg-[#F3E9E8] font-bold -mt-1 mb-2`}
+    className={`border-l-4 border-[#b6070a] opacity-90 px-3 py-1 bg-[#F3E9E8] font-bold mb-1`}
   >
     {errorMessage}
   </div>
