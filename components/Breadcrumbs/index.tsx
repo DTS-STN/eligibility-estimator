dde6c5747f3d1e25df3b73fe78a7c0d8f48d84ca
Khalid Adan <KhalidAdan@users.noreply.github.com>
2022-03-14 16:40:02 -0400

    remove trailing breadcrumb (#218)

--- components/Breadcrumbs/index.tsx
+++ components/Breadcrumbs/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { BreadcrumbItem } from './BreadcrumbItem'
 
-export const Breadcrumbs: React.VFC<{ items: string[] }> = ({ items }) => {
+export const Breadcrumbs: React.VFC<{
+  items: { title: string; link: string }[]
+}> = ({ items }) => {
   return (
     <nav className="flex mt-10" aria-label="Breadcrumb">
       <ol className="inline-flex flex-col sm:flex-row items-start sm:items-center space-x-1 sm:space-x-3">
@@ -9,7 +11,7 @@ export const Breadcrumbs: React.VFC<{ items: string[] }> = ({ items }) => {
           <BreadcrumbItem
             key={index}
             showDelimiter={index < items.length - 1}
-            breadcrumb={`${breadcrumb}`}
+            breadcrumb={breadcrumb}
           />
         ))}
       </ol>
