b982d102176f3710a9ec3a26e5dec134f23fce7f
Alex Soloviev <alex.soloviev@gmail.com>
2022-04-12 12:43:45 -0400

    added 404 page with ErrorPage component from design system

---
+++ pages/404.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { WebTranslations } from '../i18n/web'
+import { useMediaQuery, useTranslation } from '../components/Hooks'
+import { NextPage } from 'next'
+import { HeadDoc } from '../components/Document'
+import { Footer } from '../components/Layout/Footer'
+import { Header } from '../components/Layout/Header'
+import { SCLabsTestHeader } from '../components/Layout/ScTestHeader'
+import { ErrorPage } from '@dts-stn/decd-design-system'
+
+const Custom404: NextPage = (props) => {
+  const router = useRouter()
+  const isMobile = useMediaQuery(400)
+  const oppositeLocale = router.locales.find((l) => l !== router.locale)
+  const tsln = useTranslation<WebTranslations>()
+  return (
+    <>
+      <HeadDoc />
+      <>
+        <SCLabsTestHeader />
+        <main>
+          <div className="mx-4 min-h-screen">
+            <div className="sm:container mx-auto">
+              <div className="flex justify-end my-4">
+                <Link
+                  href={router.asPath}
+                  locale={oppositeLocale}
+                  passHref={true}
+                >
+                  <button className="btn-link btn underline">
+                    {isMobile ? tsln.otherLangCode : tsln.otherLang}
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <Header />
+            <div className="bg-primary -mx-4">
+              <div className="flex flex-row justify-between items-center sm:container mx-auto">
+                <h3 className="text-h3 py-3 text-white font-bold px-4 md:px-0">
+                  {tsln.menuTitle}
+                </h3>
+                <p />
+              </div>
+            </div>
+            <div className="sm:container mx-auto flex flex-col mb-16 mt-8">
+              <ErrorPage lang={router.locale} errType="404" isAuth={false} />
+            </div>
+          </div>
+          <Footer />
+        </main>
+      </>
+    </>
+  )
+}
+
+export default Custom404
