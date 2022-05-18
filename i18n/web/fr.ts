4680c67c797e5652786d129217458b70589eacc2
Alex Yan <alexunique0519@gmail.com>
2022-05-18 09:47:52 -0400

    change variable name

--- i18n/web/fr.ts
+++ i18n/web/fr.ts
@@ -1,7 +1,7 @@
 // noinspection SpellCheckingInspection
 import { WebTranslations } from '.'
 import { Language, Locale } from '../../utils/api/definitions/enums'
-import api_translation from '../api/fr'
+import apiTranslation from '../api/fr'
 
 const fr: WebTranslations = {
   _language: Language.FR,
@@ -68,18 +68,18 @@ const fr: WebTranslations = {
   whatBenefitsTheEstimatorIsFor: "Prestations incluses dans l'estimateur",
   benefitAvailable:
     'Une prestation disponible aux personnes de 65 ans et plus.',
-  learnMoreAboutOldAgeSecurity: `<a className="underline text-default-text" href="${api_translation.links.oasOverview.url}" target="_blank">En savoir plus sur la Sécurité de la vieillesse</a>`,
+  learnMoreAboutOldAgeSecurity: `<a className="underline text-default-text" href="${apiTranslation.links.oasOverview.url}" target="_blank">En savoir plus sur la Sécurité de la vieillesse</a>`,
   gisDefinationText:
     'Une prestation disponible aux personnes qui reçoivent la Sécurité de la vieillesse, ont 65 ans et plus, ont un faible revenu, et habitent au Canada.',
-  learnMoreAboutGIS: `<a className="underline text-default-text" href="${api_translation.links.gisOverview.url}" target="_blank">En savoir plus sur le Supplément de revenu garanti </a>`,
+  learnMoreAboutGIS: `<a className="underline text-default-text" href="${apiTranslation.links.gisOverview.url}" target="_blank">En savoir plus sur le Supplément de revenu garanti </a>`,
   alwDefinationText:
     'Une prestation disponible aux personnes âgées entre 60 et 64 ans ayant un faible revenu et dont le conjoint reçoit le Supplément de revenu garanti.',
-  learnMoreAboutAlw: `<a className="underline text-default-text" href="${api_translation.links.alwOverview.url}" target="_blank">En savoir plus sur l'Allocation</a>`,
+  learnMoreAboutAlw: `<a className="underline text-default-text" href="${apiTranslation.links.alwOverview.url}" target="_blank">En savoir plus sur l'Allocation</a>`,
   afsDefinationText:
     'Une prestation disponible aux personnes âgées entre entre 60 et 64 ans ayant un faible revenu et dont le conjoint est décédé.',
-  learnMoreAboutAfs: `<a className="underline text-default-text" href="${api_translation.links.afsOverview.url}" target="_blank">En savoir plus sur l'Allocation au survivant</a>`,
+  learnMoreAboutAfs: `<a className="underline text-default-text" href="${apiTranslation.links.afsOverview.url}" target="_blank">En savoir plus sur l'Allocation au survivant</a>`,
   notIncludeCPP: `Cet estimateur n'inclut pas le Régime de pensions du Canada.`,
-  learnMoreAboutCpp: `<a className="underline text-default-text" href="${api_translation.links.cpp.url}" target="_blank">En savoir plus sur le Régime de pensions du Canada</a>`,
+  learnMoreAboutCpp: `<a className="underline text-default-text" href="${apiTranslation.links.cpp.url}" target="_blank">En savoir plus sur le Régime de pensions du Canada</a>`,
   aboutResultText: 'À propos des estimations',
   resultDefination: `Les estimations ne sont pas des décisions finales. Pour une évaluation plus précise de votre admissibilité, veuillez <a className='text-default-text underline' target='_blank' href='https://www.canada.ca/fr/emploi-developpement-social/ministere/coordonnees/sv.html'>communiquer avec Service Canada.</a> Les estimations ne doivent pas être considérées comme des conseils financiers.`,
   privacyHeading: 'Confidentialité',
