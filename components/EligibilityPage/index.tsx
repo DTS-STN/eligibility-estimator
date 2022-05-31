57325f69efb0711289e581a7b87fa18eff941e31
Jeremy Kennedy <jerekennedy@deloitte.ca>
2022-05-31 18:03:21 -0400

    pull out generateChildren from generateCards, for easier readability

--- components/EligibilityPage/index.tsx
+++ components/EligibilityPage/index.tsx
@@ -131,132 +131,123 @@ export const EligibilityPage: React.VFC = observer(({}) => {
     })
   }
 
+  function generateChildren(step, keys) {
+    const fields = form.fields.filter((field) => keys.includes(field.key))
+    const children = fields.map((field) => {
+      return (
+        <div key={field.key}>
+          {field.type === FieldType.NUMBER && (
+            <div className="pb-4">
+              <NumberField
+                type={field.type}
+                name={field.key}
+                label={field.label}
+                placeholder={field.placeholder ?? ''}
+                onChange={debounce((e) => handleOnChange(step, field, e), 500)}
+                value={field.value}
+                helpText={field.helpText}
+                required
+              />
+            </div>
+          )}
+          {field.type == FieldType.CURRENCY && (
+            <div className="pb-4">
+              <CurrencyField
+                type={field.type}
+                name={field.key}
+                label={field.label}
+                onChange={debounce((e) => handleOnChange(step, field, e), 500)}
+                placeholder={field.placeholder ?? ''}
+                value={field.value}
+                helpText={field.helpText}
+                required
+              />
+            </div>
+          )}
+          {field.type == FieldType.STRING && (
+            <div className="pb-4">
+              <TextField
+                type={field.type}
+                name={field.key}
+                label={field.label}
+                placeholder={field.placeholder ?? ''}
+                onChange={debounce((e) => handleOnChange(step, field, e), 500)}
+                value={field.value}
+                error={field.error}
+                required
+              />
+            </div>
+          )}
+          {(field.type == FieldType.DROPDOWN ||
+            field.type == FieldType.DROPDOWN_SEARCHABLE) && (
+            <div className="pb-4">
+              <FormSelect
+                name={field.key}
+                field={field}
+                placeholder={getPlaceholderForSelect(field, tsln)}
+                value={null}
+              />
+            </div>
+          )}
+          {(field.type == FieldType.RADIO ||
+            field.type == FieldType.BOOLEAN) && (
+            <div className="pb-4">
+              <Radio
+                name={field.key}
+                checkedValue={field.value}
+                values={
+                  field.type == 'boolean'
+                    ? [
+                        {
+                          key: 'true',
+                          text: tsln.yes,
+                        },
+                        {
+                          key: 'false',
+                          text: tsln.no,
+                        },
+                      ]
+                    : field.options
+                }
+                keyforid={field.key}
+                label={field.label}
+                onChange={(e) => handleOnChange(step, field, e)}
+                required
+              />
+            </div>
+          )}
+          {field.error && (
+            <div className="mt-6 md:pr-12">
+              <Message
+                id={field.key}
+                alert_icon_id={field.key}
+                alert_icon_alt_text="warning icon"
+                type="warning"
+                message_heading={field.error}
+                message_body={field.error}
+              />
+            </div>
+          )}
+          {field.info && (
+            <div className="mt-6 md:pr-12">
+              <Message
+                id={field.key}
+                alert_icon_id={field.key}
+                alert_icon_alt_text="info icon"
+                type="info"
+                message_heading={field.info}
+                message_body={field.info}
+              />
+            </div>
+          )}
+        </div>
+      )
+    })
+
+    return { [step]: children }
+  }
+
   function generateCards() {
-    function generateChildren(step, keys) {
-      const fields = form.fields.filter((field) => keys.includes(field.key))
-      const children = fields.map((field) => {
-        return (
-          <div key={field.key}>
-            {field.type === FieldType.NUMBER && (
-              <div className="pb-4">
-                <NumberField
-                  type={field.type}
-                  name={field.key}
-                  label={field.label}
-                  placeholder={field.placeholder ?? ''}
-                  onChange={debounce(
-                    (e) => handleOnChange(step, field, e),
-                    500
-                  )}
-                  value={field.value}
-                  helpText={field.helpText}
-                  required
-                />
-              </div>
-            )}
-            {field.type == FieldType.CURRENCY && (
-              <div className="pb-4">
-                <CurrencyField
-                  type={field.type}
-                  name={field.key}
-                  label={field.label}
-                  onChange={debounce(
-                    (e) => handleOnChange(step, field, e),
-                    500
-                  )}
-                  placeholder={field.placeholder ?? ''}
-                  value={field.value}
-                  helpText={field.helpText}
-                  required
-                />
-              </div>
-            )}
-            {field.type == FieldType.STRING && (
-              <div className="pb-4">
-                <TextField
-                  type={field.type}
-                  name={field.key}
-                  label={field.label}
-                  placeholder={field.placeholder ?? ''}
-                  onChange={debounce(
-                    (e) => handleOnChange(step, field, e),
-                    500
-                  )}
-                  value={field.value}
-                  error={field.error}
-                  required
-                />
-              </div>
-            )}
-            {(field.type == FieldType.DROPDOWN ||
-              field.type == FieldType.DROPDOWN_SEARCHABLE) && (
-              <div className="pb-4">
-                <FormSelect
-                  name={field.key}
-                  field={field}
-                  placeholder={getPlaceholderForSelect(field, tsln)}
-                  value={null}
-                />
-              </div>
-            )}
-            {(field.type == FieldType.RADIO ||
-              field.type == FieldType.BOOLEAN) && (
-              <div className="pb-4">
-                <Radio
-                  name={field.key}
-                  checkedValue={field.value}
-                  values={
-                    field.type == 'boolean'
-                      ? [
-                          {
-                            key: 'true',
-                            text: tsln.yes,
-                          },
-                          {
-                            key: 'false',
-                            text: tsln.no,
-                          },
-                        ]
-                      : field.options
-                  }
-                  keyforid={field.key}
-                  label={field.label}
-                  onChange={(e) => handleOnChange(step, field, e)}
-                  required
-                />
-              </div>
-            )}
-            {field.error && (
-              <div className="mt-6 md:pr-12">
-                <Message
-                  id={field.key}
-                  alert_icon_id={field.key}
-                  alert_icon_alt_text="warning icon"
-                  type="warning"
-                  message_heading={field.error}
-                  message_body={field.error}
-                />
-              </div>
-            )}
-            {field.info && (
-              <div className="mt-6 md:pr-12">
-                <Message
-                  id={field.key}
-                  alert_icon_id={field.key}
-                  alert_icon_alt_text="info icon"
-                  type="info"
-                  message_heading={field.info}
-                  message_body={field.info}
-                />
-              </div>
-            )}
-          </div>
-        )
-      })
-
-      return { [step]: children }
-    }
-
     return Object.keys(keyStepMap).map((step, index) => {
       const cardMeta = keyStepMap[step]
       const children = generateChildren(step, cardMeta.keys) // ex. ("step1", ["age"])
