edae1d0b22fda3ba447136647280363bd3273c95
Jeremy Kennedy <jerekennedy@deloitte.ca>
2022-07-12 16:12:45 -0400

    add custom on change for date picker

--- components/Forms/MonthAndYear.tsx
+++ components/Forms/MonthAndYear.tsx
@@ -1,6 +1,6 @@
 import { DatePicker } from '@dts-stn/decd-design-system'
 import type { Instance } from 'mobx-state-tree'
-import { InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes, useState } from 'react'
 import type { FormField } from '../../client-state/models/FormField'
 import { NumberField } from './NumberField'
 
@@ -20,6 +20,17 @@ export const MonthAndYear: React.VFC<MonthAndYearProps> = ({
   placeholder,
   onChange,
 }) => {
+  const [dateInput] = useState({ month: 'January', year: undefined })
+
+  const customOnChange = (e) => {
+    const fieldId = e.target.id
+    const fieldToSet = fieldId === 'datePickerYear' ? 'year' : 'month'
+    dateInput[fieldToSet] = e.target.value
+    if (dateInput.year && dateInput.month)
+      console.log('gotta push the change to api now:', dateInput)
+    else console.log('not pushing change')
+  }
+
   return (
     <>
       <div className="mb-2.5">
@@ -76,8 +87,8 @@ export const MonthAndYear: React.VFC<MonthAndYearProps> = ({
         // maxYear={2050}
         // minYear={1999}
         // onDayChange={function noRefCheck() {}}
-        onMonthChange={function noRefCheck() {}}
-        onYearChange={function noRefCheck() {}}
+        onMonthChange={customOnChange}
+        onYearChange={customOnChange}
       />
     </>
   )
