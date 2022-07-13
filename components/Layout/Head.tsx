2a3fb73ba43a593ad60d7fba79fd9f48a466f4de
Jeremy Kennedy <jerekennedy@deloitte.ca>
2022-07-12 20:25:34 -0400

    fix next error: use Next's Script component for adobe
    https://nextjs.org/docs/messages/no-sync-scripts

--- components/Layout/Head.tsx
+++ components/Layout/Head.tsx
@@ -1,4 +1,5 @@
 import NextHead from 'next/head'
+// import Script from 'next/script'
 import { WebTranslations } from '../../i18n/web'
 import { useTranslation } from '../Hooks'
 
@@ -9,7 +10,8 @@ export const Head = () => {
     <NextHead>
       <title>{tsln.title}</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <script src="https://assets.adobedtm.com/be5dfd287373/0127575cd23a/launch-913b1beddf7a-staging.min.js" />
+      {/* commented out - causing issues with page render */}
+      {/*<Script src="https://assets.adobedtm.com/be5dfd287373/0127575cd23a/launch-913b1beddf7a-staging.min.js" />*/}
     </NextHead>
   )
 }
