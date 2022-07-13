f8df0b835134c481fa725d014c816a9f35108bf5
Jeremy Kennedy <jerekennedy@deloitte.ca>
2022-07-12 20:22:20 -0400

    fix next error: move font declaration
    https://nextjs.org/docs/messages/no-page-custom-font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,12 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
