0997173ca4f65cf6bf599c8be98c91e1746e089f
Alex Yan <alexunique0519@gmail.com>
2022-07-21 16:40:22 -0400

    Update CustomCollapse.tsx

--- components/ResultsPage/CustomCollapse.tsx
+++ components/ResultsPage/CustomCollapse.tsx
@@ -9,9 +9,9 @@ export const CustomCollapse = (props) => {
     >
       <summary
         key={`summary-${id}`}
-        className="border-none pl-0 ds-text-multi-blue-blue70b ds-underline ds-py-5px ds-cursor-pointer ds-select-none"
+        className="border-none pl-0 ds-text-multi-blue-blue70b mb-[10px] ds-underline ds-cursor-pointer ds-select-none"
       >
-        {title}
+        <span className="ml-[5px]">{title}</span>
       </summary>
       <div className="border-none">{children}</div>
     </details>
