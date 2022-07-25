33cd6a1f48ed1d6596b1df2ec4d92e8962645b72
Alex Yan <alexunique0519@gmail.com>
2022-07-25 14:57:37 -0400

    feat: dynamically generate links with state and benefit results

--- components/ResultsPage/index.tsx
+++ components/ResultsPage/index.tsx
@@ -3,18 +3,75 @@ import { useRouter } from 'next/router'
 import { useRef } from 'react'
 import { FieldInput } from '../../client-state/InputHelper'
 import { WebTranslations } from '../../i18n/web'
-import { ResultKey } from '../../utils/api/definitions/enums'
+import { ResultKey, SummaryState } from '../../utils/api/definitions/enums'
 import {
   BenefitResult,
   BenefitResultsObject,
   SummaryObject,
 } from '../../utils/api/definitions/types'
+import Tbl1SingleScraper from '../../utils/api/scrapers/tbl1SingleScraper'
 import { useTranslation } from '../Hooks'
 import { BenefitCards } from './BenefitCards'
 import { EstimatedTotal } from './EstimatedTotal'
 import { ListLinks } from './ListLinks'
 import { MayBeEligible } from './MayBeEligible'
 import { YourAnswers } from './YourAnswers'
+import { numberToStringCurrency } from '../../i18n/api'
+
+const notEligibleState = [
+  SummaryState.AVAILABLE_INELIGIBLE,
+  SummaryState.MORE_INFO,
+  SummaryState.AVAILABLE_INELIGIBLE,
+]
+
+// get the link text by current summary state
+const getEligibleLinkText: string = (
+  currentState: SummaryState,
+  tsln: WebTranslations
+) => {
+  if (notEligibleState.includes(currentState)) {
+    return tsln.resultsPage.youAreNotEligible
+  } else {
+    return tsln.resultsPage.youMayBeEligible
+  }
+}
+
+const getEstimatedMonthlyTotalLinkText: string = (
+  entitlementSum: number,
+  tsln: WebTranslations
+) => {
+  if (entitlementSum !== 0) {
+    return `${tsln.resultsPage.yourEstimatedTotal}${numberToStringCurrency(
+      entitlementSum,
+      tsln._language
+    )}`
+  }
+  return ''
+}
+
+const getNextStepLinkText: string = (
+  results: BenefitResultsObject,
+  tsln: WebTranslations
+) => {
+  for (const key in results) {
+    if (results[key]['eligibility']?.result === ResultKey.ELIGIBLE) {
+      return tsln.resultsPage.nextSteps
+    }
+  }
+  return ''
+}
+
+const getBenefitsYouMayNotEligibleForLinkText: string = (
+  results: BenefitResultsObject,
+  tsln: WebTranslations
+) => {
+  for (const key in results) {
+    if (results[key]['eligibility']?.result === ResultKey.INELIGIBLE) {
+      return tsln.resultsPage.youMayNotBeEligible
+    }
+  }
+  return ''
+}
 
 const ResultsPage: React.VFC<{
   inputs: FieldInput[]
@@ -25,13 +82,22 @@ const ResultsPage: React.VFC<{
   const tsln = useTranslation<WebTranslations>()
   const router = useRouter()
 
-  const listLinks: { text: string; url: string }[] = [
-    { text: tsln.resultsPage.youMayBeEligible, url: '#eligible' },
-    { text: tsln.resultsPage.yourEstimatedTotal, url: '#estimated' },
+  let listLinks: { text: string; url: string }[] = [
+    { text: getEligibleLinkText(summary.state, tsln), url: '#eligible' },
+    {
+      text: getEstimatedMonthlyTotalLinkText(summary.entitlementSum, tsln),
+      url: '#estimated',
+    },
     { text: tsln.resultsPage.whatYouToldUs, url: '#answers' },
-    { text: tsln.resultsPage.nextSteps, url: '#nextSteps' },
-    { text: tsln.resultsPage.youMayNotBeEligible, url: '#notEligible' },
+    { text: getNextStepLinkText(results, tsln), url: '#nextSteps' },
+    {
+      text: getBenefitsYouMayNotEligibleForLinkText(results, tsln),
+      url: '#notEligible',
+    },
   ]
+
+  // filtered out the link item which text is empty.
+  listLinks = listLinks.filter((ll) => ll.text)
 
   const resultsArray: BenefitResult[] = Object.keys(results).map(
     (value) => results[value]
