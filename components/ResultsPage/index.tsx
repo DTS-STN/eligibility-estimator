d606432ac9bbb53061a870f890fa0a061d36bef6
Alex Yan <alexunique0519@gmail.com>
2022-08-01 10:35:30 -0400

    Update index.tsx

--- components/ResultsPage/index.tsx
+++ components/ResultsPage/index.tsx
@@ -25,14 +25,12 @@ const notEligibleState = [
 
 // get the link text by current summary state
 const getEligibleLinkText = (
-  currentState: SummaryState,
+  entitlementSum: number,
   tsln: WebTranslations
 ): string => {
-  if (notEligibleState.includes(currentState)) {
-    return tsln.resultsPage.youAreNotEligible
-  } else {
-    return tsln.resultsPage.youMayBeEligible
-  }
+  return entitlementSum > 0
+    ? tsln.resultsPage.youMayBeEligible
+    : tsln.resultsPage.youAreNotEligible
 }
 
 const getEstimatedMonthlyTotalLinkText = (
@@ -49,27 +47,27 @@ const getEstimatedMonthlyTotalLinkText = (
 }
 
 const getNextStepLinkText = (
-  results: BenefitResultsObject,
+  resultsArray: BenefitResult[],
   tsln: WebTranslations
 ): string => {
-  for (const key in results) {
-    if (results[key]['eligibility']?.result === ResultKey.ELIGIBLE) {
-      return tsln.resultsPage.nextSteps
-    }
-  }
-  return ''
+  const EligibleBenefits = resultsArray.filter((r) =>
+    [ResultKey.ELIGIBLE, ResultKey.INCOME_DEPENDENT].includes(
+      r.eligibility.result
+    )
+  )
+  return EligibleBenefits.length > 0 ? tsln.resultsPage.nextSteps : ''
 }
 
 const getBenefitsYouMayNotEligibleForLinkText = (
-  results: BenefitResultsObject,
+  resultsArray: BenefitResult[],
   tsln: WebTranslations
 ): string => {
-  for (const key in results) {
-    if (results[key]['eligibility']?.result === ResultKey.INELIGIBLE) {
-      return tsln.resultsPage.youMayNotBeEligible
-    }
-  }
-  return ''
+  const notEligibleBenefits = resultsArray.filter(
+    (r) => r.eligibility.result === ResultKey.INELIGIBLE
+  )
+  return notEligibleBenefits.length > 0
+    ? tsln.resultsPage.youMayNotBeEligible
+    : ''
 }
 
 const ResultsPage: React.VFC<{
@@ -81,26 +79,32 @@ const ResultsPage: React.VFC<{
   const tsln = useTranslation<WebTranslations>()
   const router = useRouter()
 
-  let listLinks: { text: string; url: string }[] = [
-    { text: getEligibleLinkText(summary.state, tsln), url: '#eligible' },
+  const resultsArray: BenefitResult[] = Object.keys(results).map(
+    (value) => results[value]
+  )
+
+  let listLinks: {
+    text: string
+    url: string
+  }[] = [
+    {
+      text: getEligibleLinkText(summary.entitlementSum, tsln),
+      url: '#eligible',
+    },
     {
       text: getEstimatedMonthlyTotalLinkText(summary.entitlementSum, tsln),
       url: '#estimated',
     },
     { text: tsln.resultsPage.whatYouToldUs, url: '#answers' },
-    { text: getNextStepLinkText(results, tsln), url: '#nextSteps' },
+    { text: getNextStepLinkText(resultsArray, tsln), url: '#nextSteps' },
     {
-      text: getBenefitsYouMayNotEligibleForLinkText(results, tsln),
+      text: getBenefitsYouMayNotEligibleForLinkText(resultsArray, tsln),
       url: '#notEligible',
     },
   ]
 
   // filtered out the link item which text is empty.
   listLinks = listLinks.filter((ll) => ll.text)
-
-  const resultsArray: BenefitResult[] = Object.keys(results).map(
-    (value) => results[value]
-  )
 
   const resultsEligible: BenefitResult[] = resultsArray.filter(
     (result) =>
