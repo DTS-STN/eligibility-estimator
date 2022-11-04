485524e75f202c4560bb3741d13f9300dfbffaa3
Alex Yan <alexunique0519@gmail.com>
2022-11-04 16:41:19 -0400

    feat: add acronym of Old Age Security based on scenarios

--- components/ResultsPage/MayBeEligible.tsx
+++ components/ResultsPage/MayBeEligible.tsx
@@ -10,6 +10,22 @@ export const MayBeEligible: React.VFC<{
   const tsln = useTranslation<WebTranslations>()
   const apiTrans = getTranslations(tsln._language)
   const isEligible: boolean = resultsEligible.length > 0
+  const titleArray = [
+    'Old Age Security pension',
+    'Pension de la Sécurité de la vieillesse',
+  ]
+  const titleWithAcronymArray = [
+    'Old Age Security (OAS) pension',
+    'Pension de la Sécurité de la vieillesse (SV)',
+  ]
+
+  const transformBenefitName = (benefitName) => {
+    let benefitText = ''
+    const foundIndex = titleArray.findIndex((t) => t === benefitName)
+    benefitText =
+      foundIndex != -1 ? titleWithAcronymArray[foundIndex] : benefitName
+    return benefitText
+  }
 
   return (
     <>
@@ -35,7 +51,7 @@ export const MayBeEligible: React.VFC<{
         <ul className="pl-5 list-disc text-content font-semibold">
           {resultsEligible.map((benefit) => (
             <li key={benefit.benefitKey}>
-              {apiTrans.benefit[benefit.benefitKey]}
+              {transformBenefitName(apiTrans.benefit[benefit.benefitKey])}
             </li>
           ))}
         </ul>
