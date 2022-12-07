244e944ea902f5a43a6086bede31d2ece3be7c80
Alex Yan <alexunique0519@gmail.com>
2022-12-07 11:58:28 -0500

    removed the extra hyphen

--- i18n/api/fr.ts
+++ i18n/api/fr.ts
@@ -75,7 +75,7 @@ const fr: Translations = {
     [FieldKey.PARTNER_YEARS_IN_CANADA_SINCE_18]:
       "Depuis l'âge de 18 ans, combien d'années votre conjoint a-t-il habité au Canada?",
     [FieldKey.PARTNER_EVER_LIVED_SOCIAL_COUNTRY]:
-      'Votre conjoint-a-t-il déjà vécu dans un pays ayant un {LINK_SOCIAL_AGREEMENT} avec le Canada?',
+      'Votre conjoint a-t-il déjà vécu dans un pays ayant un {LINK_SOCIAL_AGREEMENT} avec le Canada?',
   },
   questionShortText: {
     [FieldKey.INCOME_AVAILABLE]: 'Revenu fourni',
