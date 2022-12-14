f013d7fb269d7c0be54735af80158148c00ae84f
Yan Yan <alexunique0519@gmail.com>
2022-12-14 14:07:13 -0500

    remove the old one

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,11 +10,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         />
-        <script
-          src="https://code.jquery.com/jquery-3.6.2.min.js"
-          integrity="sha256-2krYZKh//PcchRtd+H+VyyQoZ/e3EcrkxhM8ycwASPA="
-          crossOrigin="anonymous"
-        ></script>
       </Head>
       <body>
         <Main />
