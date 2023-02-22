f5f48a20cf8c755a6b04e66d8f586c3cf0ccf58c
MarcoGoC <marcogoc951@gmail.com>
2023-02-22 16:03:26 -0500

    undo commented code as requested by Andreanne

--- utils/api/benefits/oasBenefit.ts
+++ utils/api/benefits/oasBenefit.ts
@@ -277,25 +277,25 @@ export class OasBenefit extends BaseBenefit<EntitlementResultOas> {
       return cardCollapsedText
     }
 
-    // // increase at 75
-    // if (this.currentEntitlementAmount !== this.age75EntitlementAmount)
-    //   cardCollapsedText.push(
-    //     this.translations.detailWithHeading.oasIncreaseAt75
-    //   )
-    // else
-    //   cardCollapsedText.push(
-    //     this.translations.detailWithHeading.oasIncreaseAt75Applied
-    //   )
+    // increase at 75
+    if (this.currentEntitlementAmount !== this.age75EntitlementAmount)
+      cardCollapsedText.push(
+        this.translations.detailWithHeading.oasIncreaseAt75
+      )
+    else
+      cardCollapsedText.push(
+        this.translations.detailWithHeading.oasIncreaseAt75Applied
+      )
 
-    // // deferral
-    // if (this.deferralIncrease)
-    //   cardCollapsedText.push(
-    //     this.translations.detailWithHeading.oasDeferralApplied
-    //   )
-    // else if (this.input.age >= 65 && this.input.age < 70)
-    //   cardCollapsedText.push(
-    //     this.translations.detailWithHeading.oasDeferralAvailable
-    //   )
+    // deferral
+    if (this.deferralIncrease)
+      cardCollapsedText.push(
+        this.translations.detailWithHeading.oasDeferralApplied
+      )
+    else if (this.input.age >= 65 && this.input.age < 70)
+      cardCollapsedText.push(
+        this.translations.detailWithHeading.oasDeferralAvailable
+      )
 
     return cardCollapsedText
   }
