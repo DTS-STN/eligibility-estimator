d73db8c8f8e03f1f78443e2b3004c25c0a9d2a1b
Alex Soloviev <alex.soloviev@gmail.com>
2023-02-22 08:44:13 -0700

    ADO-107285: Remove test banner and add info message on the overview page (#562)

--- i18n/web/en.ts
+++ i18n/web/en.ts
@@ -37,6 +37,8 @@ const en: WebTranslations = {
   nextStep: 'Next step',
   getEstimate: 'Estimate my benefits',
   required: '(required)',
+  workInProgress: 'This estimator features works in progress',
+  workInProgressBody: 'Parts of this site may not work and will change.',
   homePageP1:
     'Use this estimator to find out how much money you could get from Old Age Security benefits. Please note that this is an estimator and not an application for benefits.',
   homePageHeader1: 'Who these benefits are for',
