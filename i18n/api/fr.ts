5d5fd7a00525075a725eb39fe64982b2fbdf474b
Alex Soloviev <alex.soloviev@gmail.com>
2023-02-24 13:45:00 -0700

    Removed duplicate sentence in French (#596)

--- i18n/api/fr.ts
+++ i18n/api/fr.ts
@@ -362,7 +362,7 @@ const fr: Translations = {
       serviceCanadaReviewYourPayment:
         'Si vous présentez une demande, Service Canada révisera le montant de votre paiement chaque année en fonction de votre déclaration de revenus.',
       automaticallyBePaid:
-        'Si vous présentez une demande, Service Canada révisera le montant de votre paiement chaque année en fonction de votre déclaration de revenus. Vous recevrez automatiquement des paiements si votre revenu vous rend admissible.',
+        'Vous recevrez automatiquement des paiements si votre revenu vous rend admissible.',
       youShouldReceiveLetter:
         "Vous devriez recevoir une lettre au sujet de votre statut d'inscription le mois après votre 64e anniversaire.",
       youShouldHaveReceivedLetter:
