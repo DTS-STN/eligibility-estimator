6c1e42b1440487d29c1dc9e29e849a9ed6212903
Alex Soloviev <alex.soloviev@gmail.com>
2023-03-22 11:25:10 -0700

    add notes

--- load_test.js
+++ load_test.js
@@ -2,6 +2,13 @@
 To run load test:
 1. Install k6 (https://k6.io/docs/get-started/installation/)
 2. Run command: `k6 run load_test.js` from main directory
+
+Notes:
+Apart from using stages (current test), load tests were tried with 100-1000 users with 10s duration 
+to test both the duration of requests with varying loads as well as the point at which we start to receive 
+non 200 status codes. After 200 simultaneous users, the request duration reaches 10-12 seconds but we see no
+failures until 600 conssequent users at which point the request duration jumps to 20 seconds and we see our first
+failure. However, even at this number, 99% of all requests return a 200 status code.
 */
 
 import http from 'k6/http'
