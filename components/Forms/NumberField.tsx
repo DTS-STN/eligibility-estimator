e5636ff94ba8c2416bf47f4d25b37146133a9f7d
Maxim Lam <maxim.lam@hrsdc-rhdcc.gc.ca>
2023-03-22 10:00:17 -0400

    Made the age fields shorter in length

--- components/Forms/NumberField.tsx
+++ components/Forms/NumberField.tsx
@@ -53,7 +53,7 @@ export const NumberField: React.VFC<NumberFieldProps> = ({
       <NumberFormat
         id={`enter-${name}`}
         name={name}
-        className={`form-control text-content border-form-border ${
+        className={`form-control text-content border-form-border w-24 ${
           error ? ' !border-danger' : ''
         }`}
         data-testid="number-input"
