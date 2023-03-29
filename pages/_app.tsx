01e229b51d8efb858230486ba62a3c250a085290
MarcoGoC <marcogoc951@gmail.com>
2023-03-29 08:54:52 -0400

    removing auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
+//import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
-import Auth from './Auth'
+//import Auth from './Auth'
 
 const PRIVATE_PATHS = ['/', '/eligibility', '/results']
 
@@ -12,18 +12,18 @@ function MyApp({
 }: AppProps) {
   const AuthRequired = PRIVATE_PATHS.some((path) => route.startsWith(path))
   return (
-    <SessionProvider session={session}>
-      {/*<Script src="/scripts/adobe.js" />*/}
-      {/* the below line appears to crash the page - to investigate if it's even needed at all */}
-      {/*<script type="text/javascript">_satellite.pageBottom()</script>*/}
-      {AuthRequired ? (
-        <Auth>
-          <Component {...pageProps} />
-        </Auth>
-      ) : (
-        <Component {...pageProps} />
-      )}
-    </SessionProvider>
+    //    <SessionProvider session={session}>
+    //      {/*<Script src="/scripts/adobe.js" />*/}
+    //      {/* the below line appears to crash the page - to investigate if it's even needed at all */}
+    //      {/*<script type="text/javascript">_satellite.pageBottom()</script>*/}
+    //      {AuthRequired ? (
+    //        <Auth>
+    <Component {...pageProps} />
+    //        </Auth>
+    //      ) : (
+    //        <Component {...pageProps} />
+    //      )}
+    //    </SessionProvider>
   )
 }
 
