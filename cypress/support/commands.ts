7c1ba9cd4fb412542c68f9c705d49bc47fea1e1e
Alex Soloviev <alex.soloviev@gmail.com>
2023-04-25 08:01:15 -0700

    typescript fix

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -23,3 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Prevent TypeScript from reading file as legacy script
+export {}
