3655fdd44d8463bf3874682116cc3ff20345f5d5
Alex Soloviev <alex.soloviev@gmail.com>
2023-05-05 11:49:39 -0700

    added years and months selects to Duration component

--- components/Forms/Duration.tsx
+++ components/Forms/Duration.tsx
@@ -18,6 +18,10 @@ const Duration: FC<DurationProps> = ({
   requiredText,
   error,
 }) => {
+  const validationClass = !!error
+    ? 'ds-border-specific-red-red50b focus:ds-border-multi-blue-blue60f focus:ds-shadow-text-input'
+    : 'ds-border-multi-neutrals-grey85a focus:ds-border-multi-blue-blue60f focus:ds-shadow-text-input'
+
   return (
     <fieldset>
       <legend>
@@ -30,7 +34,45 @@ const Duration: FC<DurationProps> = ({
           fieldId={`enter-${name}`}
         />
       </legend>
-      <div>TEST</div>
+      <div className="datePicker ds-relative ds-flex">
+        <div className="flex flex-col">
+          <label className="ds-form-date" htmlFor="duration-years">
+            Years
+          </label>
+
+          <select
+            id="duration-years"
+            defaultValue={0}
+            onChange={() => console.log('testing select')}
+            className={`ds-w-165px ds-py-5px ds-flex ds-px-14px ds-date-text ds-border-1.5 ds-border-multi-neutrals-grey85a ds-rounded ${validationClass}`}
+          >
+            {[...Array(10).keys()].map((mv, index) => (
+              <option value={mv} key={`datePicker-month-option-${index}`}>
+                {mv}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="flex flex-col sm:ds-pl-24px ds-pl-8px">
+          <label className="ds-form-date" htmlFor="duration-months">
+            Months
+          </label>
+
+          <select
+            id="duration-months"
+            defaultValue={0}
+            onChange={() => console.log('testing select')}
+            className={`ds-w-165px ds-py-5px ds-flex ds-px-14px ds-date-text ds-border-1.5 ds-border-multi-neutrals-grey85a ds-rounded ${validationClass}`}
+          >
+            {[...Array(10).keys()].map((mv, index) => (
+              <option value={mv} key={`datePicker-month-option-${index}`}>
+                {mv}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
     </fieldset>
   )
 }
