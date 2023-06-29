ac57e35c6c995460baadc0924abd1a5592259547
MarcoGoC <marcogoc951@gmail.com>
2023-06-29 15:04:32 -0400

    adding csp source

--- next.config.js
+++ next.config.js
@@ -9,13 +9,18 @@ module.exports = {
   // .
   async headers() {
     return [
-      { key: 'X-DNS-Prefetch-Control', value: 'on' },
-      { key: 'X-Frame-Options', value: 'DENY' },
-      { key: 'X-Content-Type-Options', value: 'nosniff' },
-      { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
       {
-        key: 'Strict-Transport-Security',
-        value: 'max-age=63072000; includeSubDomains; preload',
+        source: '/:path*',
+        headers: [
+          { key: 'X-DNS-Prefetch-Control', value: 'on' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+        ],
       },
     ]
   },
