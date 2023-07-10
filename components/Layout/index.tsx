9a3ca6db1e40dd200ed2c908f7aa72ef9a695746
MarcoGoC <marcogoc951@gmail.com>
2023-07-10 12:50:31 -0400

    adding change from i18n

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -17,8 +17,15 @@ export const Layout: React.VFC<{
 }> = ({ children, title }) => {
   const router = useRouter()
 
+  // basically returns 'results' or 'resultats' IF, otherwise index or questions in the other locale.
   const langToggleLink =
-    router.locale === 'fr' ? `/en${router.asPath}` : `/fr${router.asPath}`
+    router.asPath === '/results' && router.locale === 'en'
+      ? '/fr/resultats'
+      : router.asPath === '/resultats' && router.locale === 'fr'
+      ? '/en/results'
+      : router.locale === 'en'
+      ? `/fr${router.asPath}`
+      : `/en${router.asPath}`
 
   const tsln = useTranslation<WebTranslations>()
 
