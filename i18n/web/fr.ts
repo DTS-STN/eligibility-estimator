91fa2a440072da0ba9b4653c07aac66ba91d3606
MarcoGoC <marcogoc951@gmail.com>
2023-07-14 09:45:17 -0400

    change disclaimer

--- i18n/web/fr.ts
+++ i18n/web/fr.ts
@@ -143,7 +143,7 @@ const fr: WebTranslations = {
   },
   resultsPage: {
     header: "Tableau des résultats d'estimation",
-    general: `Les résultats suivants ne sont qu'une estimation de votre admissibilité et de votre paiement mensuel. Des changements dans votre situation pourraient modifier vos résultats.`,
+    general: `Les résultats suivants ne sont qu'une estimation de votre admissibilité et de vos paiements mensuels <span style="font-weight: bold;">basée sur les montants actuels</span>. Ceux-ci peuvent augmenter avec le coût de la vie. Des changements dans votre situation pourraient aussi modifier vos résultats`,
     onThisPage: 'Sur cette page',
     tableHeader1: 'Prestations',
     tableHeader2: 'Montant mensuel estimé (CAD)',
