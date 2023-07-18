bf6317e5881997f9c662514852595aac0440936f
MarcoGoC <marcogoc951@gmail.com>
2023-07-18 07:25:18 -0400

    removing private paths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,13 @@ import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
 import Auth from '../components/Layout/Auth'
 
-const PRIVATE_PATHS = ['/', '/questions', '/results', '/resultats']
-
 function MyApp({
   Component,
   pageProps: { session, ...pageProps },
   router: { route },
 }: AppProps) {
   const AuthRequired =
-    process.env.APP_ENV !== 'production' &&
-    process.env.APP_ENV !== 'alpha' &&
-    PRIVATE_PATHS.some((path) => route.startsWith(path))
+    process.env.APP_ENV !== 'production' && process.env.APP_ENV !== 'alpha'
 
   return (
     <>
