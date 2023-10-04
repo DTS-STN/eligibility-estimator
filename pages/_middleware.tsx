09cb2bfe5d21f83ce8947f1d6a1706075e306950
MarcoGoC <marcogoc951@gmail.com>
2023-10-04 09:31:56 -0400

    blocking api calls in production

---
+++ pages/_middleware.tsx
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  //
+  const AuthRequired =
+    process.env.APP_ENV === 'production' || process.env.APP_ENV === 'alpha'
+
+  const url = request.nextUrl
+  const { pathname } = url
+
+  if (AuthRequired) {
+    if (pathname.startsWith(`/api/`)) {
+      if (
+        !request.headers
+          .get('referer')
+          ?.includes('estimateursv-oasestimator.service.canada.ca')
+      ) {
+        return NextResponse.redirect(new URL('/', request.url))
+      }
+    }
+  }
+
+  return NextResponse.next()
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/((?!_next|fonts|examples|svg|[\\w-]+\\.\\w+).*)'],
+}
