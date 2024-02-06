1f854ecaf2dcebb8d6f06a6b999acd248a272f96
Alex Soloviev <alex.soloviev@gmail.com>
2024-02-06 12:59:02 -0500

    span is a strong element for accessibility purposes

--- i18n/web/en.ts
+++ i18n/web/en.ts
@@ -308,7 +308,7 @@ const en: WebTranslations = {
   resultsPage: {
     header: 'Table of estimated monthly amounts',
     general:
-      'The following is only an estimate of your eligibility and monthly payments <span style="font-weight: bold;">based on current rates</span>. Amounts may increase with the cost of living. Changes in your circumstances may also impact your results.',
+      'The following is only an estimate of your eligibility and monthly payments <strong style="font-weight: bold;">based on current rates</strong>. Amounts may increase with the cost of living. Changes in your circumstances may also impact your results.',
     onThisPage: 'On this page',
     tableHeader1: 'Benefit',
     tableHeader2: 'Estimated monthly amount (CAD)',
