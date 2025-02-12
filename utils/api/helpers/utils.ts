ecfbf435dd58c292970776c5b6dd4c0e13f3e810
Alex Soloviev <alex.soloviev@gmail.com>
2025-02-12 12:18:39 -0700

    save

--- utils/api/helpers/utils.ts
+++ utils/api/helpers/utils.ts
@@ -232,7 +232,7 @@ export function OasEligibility(
 
     yearsOfResAtEligibility =
       livingCountry == 'CAN'
-        ? Math.floor(ageOfEligibility - ageAtStart + yearsInCanadaAtStart)
+        ? ageOfEligibility - ageAtStart + yearsInCanadaAtStart
         : yearsInCanadaAtStart
   }
 
