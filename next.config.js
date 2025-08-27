1b36ea7405450b47163144c4913a64d1228eeec1
amy.wong <amy.wong@hrsdc-rhdcc.gc.ca>
2025-08-27 10:59:39 -0400

    adding key to allow all origins

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ module.exports = {
           { key: 'X-DNS-Prefetch-Control', value: 'on' },
           { key: 'X-Frame-Options', value: 'DENY' },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
           {
             key: 'Strict-Transport-Security',
